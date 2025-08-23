e24195d37bb966f37c512d97cce2faab9d851088
k-nanchi <kaito@siba-service.jp>
2025-08-23 16:13:08 +0900

    init project

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+      in 
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            pnpm
+            git
+            rust-bin.stable.latest.default
+            gtk3
+            libsoup_3
+            webkitgtk_4_1
+            pkg-config
+          ];
+
+          shellHook = ''
+            export GDK_BACKEND=x11
+            export WEBKIT_DISABLE_DMABUF_RENDERER=1
+          '';
+        };
+      }
+    );
+}
